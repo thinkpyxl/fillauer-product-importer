fb20f0ad568194ec830bdcad8b6efc45d02a4420
the-sides <sides798@gmail.com>
2019-10-29 10:54:39 -0400

    base procedure for comparing attributes

--- client.js
+++ client.js
@@ -17,5 +17,50 @@ function findAttrColPos(haystack, needle){
 
 let parentIdPos = findAttrColPos(products, "Parent ID")
 
+// Keyed on parent id
+let productSeries = {}
+
+function attachAttributes(attrRow, product){
+    for(let i = 0; i < prod.length; i++){
+        // Specified cells only
+        if(product[i] !== ""){
+
+        }
+    }
+}
+
+// Traverse through products and build product series
+for(let i = 1; i < products.length; i++){
+    let crntProd = products[i]
+    let prod = attachAttributes(products[0], crntProd)
+    if(productSeries[prod['Parent ID']] !== undefined){
+        // Existing product series
+
+    }
+    else{
+        // New product series
+        productSeries[prod['Parent ID']] = []
+        let prodObj = {}
+        productSeries[prod['Parent ID']].append()
+
+    }
+}
+
+// With seperated product series and complete product objects, 
+//     go by a specific series and find varying attributes.
+let seriesArray = Object.keys(productSeries)
+for(let i = 0; i < seriesArray.length; i++){
+    let series = productSeries[seriesArray[i]]
+    let variationAttrs = []
+    let prevElem = series[0]
+    // Traverse products
+    for(let j = 1; j < series.length; j++){
+        // Traverse attributes
+       for(let a = 0; a < series[j].length; a++){
+
+       }
+
+    }
+}
 
 console.log(Object.values(products))
