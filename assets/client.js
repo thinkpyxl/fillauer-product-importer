604609a0d2924e0006b59ed5d318c7395caf3790
the-sides <sides798@gmail.com>
2019-10-31 12:31:22 -0400

    Enqueuing admin scripts

--- assets/client.js
+++ assets/client.js
@@ -1,3 +1,4 @@
+console.log('client-side script executed')
 // Utitlity Functions
 function range2array(start, end) {
     if(start === end) return [start];
@@ -14,81 +15,90 @@ function removeVal(arr, toRemove){
 }
 //* ////////////////////////////////////////////////////
 
-
-const shorthandsJSON = document.getElementById('shorthand-data').textContent
-const productsJSON = document.getElementById('product-data').textContent
-
-let shorthands = JSON.parse(shorthandsJSON)
-let products = JSON.parse(productsJSON)
-const productValues = Object.values(products)
-
-// Create attribute to index lookup object
-let attributes = {}
-let attributeValues = Object.values(products[0])
-for(let i = 0; i < attributeValues.length; i++){
-    attributes[products[0][i]] = i
-}
-console.log(attributes)
-
-
-
-// Keyed on parent id
-let productSeries = {}
-let parentIdPos = attributes["Parent ID"]
-
-// Prod will be a single product's row
-function attachAttributes(attrRow, prod){
-    let prodObj = {}
-    prod = Object.values(prod)
-    for(let i = 0; i < prod.length; i++){
-        // Specified cells only
-        if(prod[i] !== ""){
-            prodObj[attrRow[i]] = prod[i]
+function run(){
+    const shorthandsElm = document.getElementById('shorthand-data')
+    const productsElm = document.getElementById('product-data')
+    
+    if(productsElm === null){
+        return false
+    }
+    const shorthandsJSON = shorthandsElm.textContent
+    const productsJSON = productsElm.textContent
+    
+    let shorthands = JSON.parse(shorthandsJSON)
+    let products = JSON.parse(productsJSON)
+    const productValues = Object.values(products)
+    
+    // Create attribute to index lookup object
+    let attributes = {}
+    let attributeValues = Object.values(products[0])
+    for(let i = 0; i < attributeValues.length; i++){
+        attributes[products[0][i]] = i
+    }
+    console.log(attributes)
+    
+    
+    
+    // Keyed on parent id
+    let productSeries = {}
+    let parentIdPos = attributes["Parent ID"]
+    
+    // Prod will be a single product's row
+    function attachAttributes(attrRow, prod){
+        let prodObj = {}
+        prod = Object.values(prod)
+        for(let i = 0; i < prod.length; i++){
+            // Specified cells only
+            if(prod[i] !== ""){
+                prodObj[attrRow[i]] = prod[i]
+            }
+        }
+        return prodObj
+    }
+    
+    
+    // Traverse through products and build product series object
+    for(let i = 1; i < productValues.length; i++){
+        let crntRow = products[i]
+        let prod = attachAttributes(attributeValues, products[i])
+    
+        // New product series
+        if(productSeries[prod['Parent ID']] === undefined){
+            productSeries[prod['Parent ID']] = []
+        }
+    
+        productSeries[prod['Parent ID']].push(prod)
+    }
+    
+    console.log(productSeries)
+    
+    // 
+    //    Product Number Generation
+    //
+    // With seperated product series and complete product objects, 
+    //     go by a specific series and find varying attributes.
+    
+    let seriesArray = Object.keys(productSeries)
+    for(let i = 0; i < seriesArray.length; i++){
+        let series = productSeries[seriesArray[i]]
+        let variationAttrs = []
+        let suspectAttr = range2array(0, attributeValues.length)
+    
+        let prevElem = series[0]
+        // Traverse products within a series
+        for(let j = 1; j < series.length; j++){
+            // Traverse attributes
+            for(let a = 0; a < series[j].length; a++){
+                // if()
+                if(series[j][a] !== prevElem[a]){
+                    confirmedVariationsIndices.append(a)
+                    suspectAttr = removeVal(suspectAttr, a)
+                }
+           }
+           prevElem = series[j]
+    
         }
     }
-    return prodObj
 }
 
-
-// Traverse through products and build product series object
-for(let i = 1; i < productValues.length; i++){
-    let crntRow = products[i]
-    let prod = attachAttributes(attributeValues, products[i])
-
-    // New product series
-    if(productSeries[prod['Parent ID']] === undefined){
-        productSeries[prod['Parent ID']] = []
-    }
-
-    productSeries[prod['Parent ID']].push(prod)
-}
-
-console.log(productSeries)
-
-// 
-//    Product Number Generation
-//
-// With seperated product series and complete product objects, 
-//     go by a specific series and find varying attributes.
-
-let seriesArray = Object.keys(productSeries)
-for(let i = 0; i < seriesArray.length; i++){
-    let series = productSeries[seriesArray[i]]
-    let variationAttrs = []
-    let suspectAttr = range2array(0, attributeValues.length)
-
-    let prevElem = series[0]
-    // Traverse products within a series
-    for(let j = 1; j < series.length; j++){
-        // Traverse attributes
-        for(let a = 0; a < series[j].length; a++){
-            // if()
-            if(series[j][a] !== prevElem[a]){
-                confirmedVariationsIndices.append(a)
-                suspectAttr = removeVal(suspectAttr, a)
-            }
-       }
-       prevElem = series[j]
-
-    }
-}
+run()
--- product-importer.php
+++ product-importer.php
@@ -4,37 +4,25 @@
  * Description: Import a CSV file to update products on the database
  */
 
+
 //  Create menu
 function importer_menu(){
     add_menu_page("Product Importer", "Product Importer", "manage_options", "product-options", "product_import_page");
     add_submenu_page("Product Importer", "Product Importer", "Product Importer", "Product Importer", "Product Importer");
 }
 
-add_action('admin_menu', 'importer_menu');
+function register_processor(){
+    wp_register_script('product-import-processor', plugins_url('assets/client.js',__FILE__), '1.0.0', true);
+    wp_enqueue_script('product-import-processor');
+}
 
-// function theme_options_panel(){
-//     add_menu_page('Theme page title', 'Theme menu label', 'manage_options', 'theme-options', 'wps_theme_func');
-//     add_submenu_page( 'theme-options', 'Settings page title', 'Settings menu label', 'manage_options', 'theme-op-settings', 'wps_theme_func_settings');
-//     add_submenu_page( 'theme-options', 'FAQ page title', 'FAQ menu label', 'manage_options', 'theme-op-faq', 'wps_theme_func_faq');
-//   }
-//   add_action('admin_menu', 'theme_options_panel');
-   
-function uploadfile(){
+add_action('admin_menu', 'importer_menu');
+add_action('admin_enqueue_scripts', 'register_processor');
+
+
+function product_import_page(){
+    do_action('admin_enqueue_scripts', 'register_processor');
+
+    echo '<h2>Welcome to my plugin. Snacks are in the back</h2>';
     include 'upload-file.php';
 }
-function product_import_page(){
-    echo '<h2>welcome to my plugin. Snacks are in the back</h2>';
-    uploadfile();
-}
-//   function wps_theme_func(){
-//           echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
-//           <h2>Theme</h2></div>';
-//   }
-//   function wps_theme_func_settings(){
-//           echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
-//           <h2>Settings</h2></div>';
-//   }
-//   function wps_theme_func_faq(){
-//           echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
-//           <h2>FAQ</h2></div>';
-//   }
