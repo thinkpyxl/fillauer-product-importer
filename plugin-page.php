fbc8fcaaf51459ea8d60e269dd3067c7959761a2
the-sides <sides798@gmail.com>
2019-11-05 11:58:58 -0500

    no need to pass php data anymore

--- plugin-page.php
+++ plugin-page.php
@@ -6,32 +6,3 @@
       <br>
       <input id='import_button' type='button' name='import_button' value='Submit'>
 </form>
-
-
-<?php
-function run_processor($file_path){
-    $file_data    = json_encode(array_map('str_getcsv', file($file_path)));
-
-    wp_register_script('product-import-processor', plugins_url('assets/client.js',__FILE__), '1.0.0', true);
-    wp_localize_script('product-import-processor', 'php_product_data', array(
-        'product_data' => $file_data,
-    ));
-    wp_enqueue_script('product-import-processor');
-    
-}
-
-// File reciever
-if(isset($_POST['import_button'])){
-
-  if($_FILES['file']['name'] != ''){
-    $uploadedfile = $_FILES['file'];
-    echo "<p>File Received: ".$uploadedfile['name']."</p>";
-    echo "<p class='import_status'>Processing...</p>";
-
-    // run_processor($uploadedfile['tmp_name']);
-    echo "<div class='results'></div>";
-  }
- 
-}
-
-?>
