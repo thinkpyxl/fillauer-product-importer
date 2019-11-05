b623b1a33462c7a060ad9cc39a169e13f0c46dfb
the-sides <sides798@gmail.com>
2019-11-05 11:58:30 -0500

    cleaner php structure

--- fillauer-product-importer.php
+++ fillauer-product-importer.php
@@ -11,20 +11,16 @@ function importer_menu(){
     add_submenu_page("Product Importer", "Product Importer", "Product Importer", "Product Importer", "Product Importer");
 }
 
-add_action('admin_menu', 'importer_menu');
 function register_processor(){
     wp_register_script('product-import-processor', plugins_url('dist/scripts/client.js',__FILE__), '1.0.0', true);
-    // wp_localize_script('product-import-processor', 'php_vars', array(
-    //     'test' => 'yay',
-    // ));
     wp_enqueue_script('product-import-processor');
 }
 
+add_action('admin_menu', 'importer_menu');
 add_action('admin_enqueue_scripts', 'register_processor');
-// do_action('admin_enqueue_scripts', 'register_processor');
 
 
 function product_import_page(){
     echo '<h2>Welcome to my plugin. Snacks are in the back</h2>';
-    include 'upload-file.php';
+    include 'plugin-page.php';
 }
