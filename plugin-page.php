4cddf52eb0a05b3c7e7c40556bff1941ed1fc3d9
the-sides <sides798@gmail.com>
2019-11-17 20:43:40 -0500

    Accept two files, parents and variations

--- plugin-page.php
+++ plugin-page.php
@@ -1,9 +1,15 @@
 <!-- Heavy help from https://makitweb.com/programmatically-file-upload-from-custom-plugin-in-wordpress/ -->
 
 <h1>Upload File</h1>
+<p>Select a parent product sheet and it's corresponding variations sheet.</p>
 <form name='product_file_importer' enctype='multipart/form-data'>
-      <input id='file_input' type='file' name='file'>
-      <br>
       <input id='test_button' type='button' name='test_button' value='Test POST'>
+      <br>
+      <label for='parent_file'>Parent Products</label>
+      <input id='parent_file_input' type='file' name='parent_file'>
+      <br>
+      <label for='variation_file'>Variation Products</label>
+      <input id='variation_file_input' type='file' name='variation_file'>
+      <br>
       <input id='import_button' type='button' name='import_button' value='Submit'>
 </form>
